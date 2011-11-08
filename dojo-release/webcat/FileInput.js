5076d84d848d1f90730afd6960e753a867319d3b
Tony Allevato <allevato@vt.edu>
2011-11-08 14:04:35 +0000

    Bug fixes in the Javascript side of WCFileInput.

--- dojo-release/webcat/FileInput.js
+++ dojo-release/webcat/FileInput.js
@@ -59,7 +59,8 @@ dojo.declare("webcat.FileInput", dojox.form.FileInputAuto,
         this.inherited(arguments);
 
         this.inputNode.validator = dojo.hitch(this, function(value, constraints) {
-            return this._hasUploaded || this.validator(value, constraints);
+            return this._hasUploaded
+                || (this.validator && this.validator(value, constraints));
         });
 
         dojo.style(this.messageNode, "vertical-align", "middle");
@@ -77,7 +78,6 @@ dojo.declare("webcat.FileInput", dojox.form.FileInputAuto,
         this.disconnect(this._blurListener);
         this.disconnect(this._focusListener);
 
-        this.overlay.style.display = "none";
         this.fakeNodeHolder.style.display = "";
 
         // summary: on click of cancel button, since we can't clear the input because of
@@ -104,6 +104,11 @@ dojo.declare("webcat.FileInput", dojox.form.FileInputAuto,
 
         this.inputNode.attr('value',
             this.clearAfterUpload ? "" : this._lastUploadedFilename);
+
+        if (this.messageNode.firstChild)
+        {
+            this.messageNode.removeChild(this.messageNode.firstChild);
+        }
 
         this._sent = false;
         this._sending = false;
@@ -133,8 +138,10 @@ dojo.declare("webcat.FileInput", dojox.form.FileInputAuto,
     // ----------------------------------------------------------
     _matchValue: function()
     {
-        this._lastUploadedFilename = this.fileInput.value;
-        this.inputNode.attr('value', this.fileInput.value);
+        var path = this.fileInput.value.replace(/^.*(\\|\/)/, '');
+
+        this._lastUploadedFilename = path;
+        this.inputNode.attr('value', path);
     },
 
 
@@ -150,7 +157,9 @@ dojo.declare("webcat.FileInput", dojox.form.FileInputAuto,
             this.messageNode.removeChild(this.messageNode.firstChild);
         }
 
-        title = dojo.string.substitute(title, { filename: this.fileInput.value });
+        title = dojo.string.substitute(title, {
+            filename: this.fileInput.value.replace(/^.*(\\|\/)/, '')
+        });
         this.messageNode.appendChild(document.createTextNode(title));
     },
 
@@ -164,10 +173,6 @@ dojo.declare("webcat.FileInput", dojox.form.FileInputAuto,
         this._sending = true;
 
         dojo.style(this.fakeNodeHolder,"display","none");
-        dojo.style(this.overlay,{
-            opacity:1,
-            display:"block"
-        });
 
         this.setMessage(this.uploadMessage);
 
@@ -208,14 +213,8 @@ dojo.declare("webcat.FileInput", dojox.form.FileInputAuto,
         this._hasUploaded = true;
         this._sent = true;
         this._sending = false;
-        dojo.style(this.overlay,{
-            opacity:0,
-            border:"none",
-            background:"none"
-        });
 
         this.spinner.stop();
-        this.overlay.style.backgroundImage = "none";
         this.fileInput.style.display = "none";
         this.fakeNodeHolder.style.display = "none";
 
@@ -234,7 +233,14 @@ dojo.declare("webcat.FileInput", dojox.form.FileInputAuto,
     // ----------------------------------------------------------
     isValid: function()
     {
-        return this.validator(null, null);
+        if (this.validator)
+        {
+            return this.validator(null, null);
+        }
+        else
+        {
+            return true;
+        }
     },
 
 
